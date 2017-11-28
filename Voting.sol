f89ea56b34f3c899e4dc4cc0c8f8817de865e0fe
Ashutosh <er.ashu.singh@gmail.com>
2017-11-29 00:53:28 +0530

    basic smart contract

--- Voting.sol
+++ Voting.sol
@@ -3,25 +3,25 @@ pragma solidity ^0.4.18;
 contract Voting {
 
     mapping (bytes32 => uint8) public votesReceived;
-    bytes32[] public candidateList;
+    bytes32[] public machineList;
 
-    function Voting(bytes32[] candidateNames) public {
-        candidateList = candidateNames;
+    function Voting(bytes32[] machineNames) public {
+        machineList = machineNames;
     }
 
-    function totalVotesFor(bytes32 candidate) view public returns (uint8) {
-        require(validCandidate(candidate));
-        return votesReceived[candidate];
+    function totalVotesFor(bytes32 machine) view public returns (uint8) {
+        require(validMachine(machine));
+        return votesReceived[machine];
     }
 
-    function voteForCandidate(bytes32 candidate) public {
-        require(validCandidate(candidate));
-        votesReceived[candidate]  += 1;
+    function voteForMachine(bytes32 machine) public {
+        require(validMachine(machine));
+        votesReceived[machine]  += 1;
     }
 
-    function validCandidate(bytes32 candidate) view public returns (bool) {
-        for(uint i = 0; i < candidateList.length; i++) {
-            if (candidateList[i] == candidate) {
+    function validMachine(bytes32 machine) view public returns (bool) {
+        for(uint i = 0; i < machineList.length; i++) {
+            if (machineList[i] == machine) {
                 return true;
             }
         }
